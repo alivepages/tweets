ec3ba297c02ab6d25ccffb699838bfb57c7bde6a
Joel <joel.fagundo@gmail.com>
2017-11-29 09:09:42 -0600

    views

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const fs = require('fs-extra');
+const ejs = require('ejs');
 const pageRouter = require('./src/routers/pageRouter.js');
 const apiRouter = require('./src/routers/apiRouter.js');
 
@@ -10,6 +11,16 @@ const PORT = process.env.PORT || 3000;
 app.use('/', pageRouter);
 app.use('/api/v1', apiRouter);
 app.use(express.static(__dirname + '/public'));
+
+// Configure EJS templatte engine
+app.engine('ejs', ejs.renderFile);
+app.set('view engine', 'ejs');
+app.set('views', `${__dirname}/src/views`);
+
+// error 404
+app.use((req, res) => {
+  res.render('404');
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
